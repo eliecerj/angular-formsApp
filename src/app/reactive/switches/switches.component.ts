d63544bada507ea0da4acc23f3e297de5adf025f
Eliecer Arteaga <eliecer.arteaga@externos.sura.cl>
2022-12-27 17:56:44 -0300

    listen valueChanges in forms

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
   styleUrls: ['./switches.component.css']
 })
-export class SwitchesComponent  {
+export class SwitchesComponent implements OnInit {
+
+  myForm: FormGroup = this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    // condiciones: [false, Validators.required]
+    condiciones: [false, Validators.requiredTrue]
+    
+  })
+
+  persona = {
+    genero: 'F',
+    notificaciones: true,
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.myForm.reset({
+      ... this.persona,
+      condiciones: false
+    });
+
+    // this.myForm.get('condiciones')?.valueChanges.subscribe (newValue => {
+    //   console.log(newValue);
+      
+    // });
+
+    this.myForm.valueChanges.subscribe( ({condiciones, ...resto}) => {
+      // delete form.condiciones;
+      this.persona = resto;
+      
+    })
+  }
+
+  guardar() {
+    const formValue = {...this.myForm.value};
+    delete formValue.condiciones;
+
+    this.persona = formValue;
+  }
+
 
   
 
