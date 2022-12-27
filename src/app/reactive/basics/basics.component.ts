d0cc29794e0fb23ddbf23bed90f4241199817779
Eliecer Arteaga <eliecerj@gmail.com>
2022-12-27 16:21:16 -0300

    reactive forms

--- src/app/reactive/basics/basics.component.ts
+++ src/app/reactive/basics/basics.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
   templateUrl: './basics.component.html',
   styleUrls: ['./basics.component.css']
 })
-export class BasicsComponent implements OnInit {
+export class BasicsComponent implements OnInit{
 
-  constructor() { }
+  myForm: FormGroup = this.fb.group({
+    nombre: [ , [Validators.required, Validators.minLength(3)]],
+    precio: [ , [Validators.required, Validators.min(0)]],
+    existencias: [ , [Validators.required, Validators.min(0)]]
+  });
 
-  ngOnInit(): void {
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.myForm.reset({
+      // this.myForm.setValue({ // con setValue hay q definir cada campo del form o da error, con el reset no
+      nombre: 'RTX 4080ti',
+      precio: 16,
+    })
   }
+
+  // myForm: FormGroup = new FormGroup({
+  //   nombre     : new FormControl('RTX 4080ti'),
+  //   precio     : new FormControl(1500),
+  //   existencias: new FormControl(5)
+  // })
+
+  campoNoEsValido(campo:string) {
+    return this.myForm.controls[campo].errors && this.myForm.controls[campo].touched
+  }
+
+  guardar() {
+
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+    this.myForm.reset();
+    
+  }
+
+
+
 
 }
--- src/app/reactive/dinamics/dinamics.component.ts
+++ src/app/reactive/dinamics/dinamics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamics',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicsComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    favoritos: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+    ], Validators.required)
+  });
+
+  get favoritosArr() {
+    return this.myForm.get('favoritos') as FormArray;
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  campoNoEsValido(campo: string) {
+    return this.myForm.controls[campo].errors && this.myForm.controls[campo].touched
+  }
+
+  guardar() {
+    if(this.myForm.valid) {
+      console.log(this.myForm.value);
+    }else{
+      this.myForm.markAllAsTouched();
+      return;
+    }
   }
 
 }
