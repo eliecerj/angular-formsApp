13ada11a80742c0c9900f3fae7658ed3c5d05603
Eliecer Arteaga <eliecer.arteaga@externos.sura.cl>
2022-12-27 19:16:01 -0300

    regex email

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  nombreApellidoPattern: string = "([a-zA-Z]+) ([a-zA-Z]+)";
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+  myForm: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.emailPattern) ]],
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.myForm.reset({
+      nombre: 'Eliecer Arteaga',
+      email: 'quemirabobo@gmail.com'
+    })
+  }
+
+  campoNoValido(campo: string) {
+    return this.myForm.get(campo)?.invalid && this.myForm.get(campo)?.touched;
+  }
+
+  guardar() {
+    this.myForm.markAllAsTouched();
   }
 
 }
